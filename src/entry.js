c010cf32beda8cad852a2f30fe6cf9724923cf62
Marek Włodarczyk <wlodarcz.marek@gmail.com>
2017-01-11 10:59:10 +0100

    obfuscated tel and email

--- src/entry.js
+++ src/entry.js
@@ -15,3 +15,16 @@ import './eventHandlers/keyDownHandler';
 import './eventHandlers/touchHandler';
 import './eventHandlers/resizeHandler';
 import './eventHandlers/menuItemClick';
+
+//obfuscate email and tel
+import {mail, tel} from './helpers/getDOMElements';
+
+mail.addEventListener('click', () => {
+    let name = 'wlodarcz.marek';
+    let vendor = '@gmail.com';
+    window.location = `mailto:${name}${vendor}`;
+});
+tel.addEventListener('click', () => {
+    let partOfNumber = '864-116';
+    window.location = `tel:+48-664-${partOfNumber}`;
+});

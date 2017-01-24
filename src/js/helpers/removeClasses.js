6ea1d694a54e41f6cbb27e00ec125d760306d923
Marek Włodarczyk <wlodarcz.marek@gmail.com>
2017-01-24 16:16:37 +0100

    fix in removeClasses function

--- src/js/helpers/removeClasses.js
+++ src/js/helpers/removeClasses.js
@@ -1,7 +1,7 @@
 //Little helper to remove classes from given element
 export default (element, ...classes) => {
   classes.forEach((classToRemove) => {
-    let regex = new RegExp(`\s?${classToRemove}`, 'g');
+    let regex = new RegExp(`\\s?${classToRemove}`, 'g');
     element.className = element.className.replace(regex, '');
   });
 }
